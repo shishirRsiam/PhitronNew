e80b7f2eec5bd9c5e961669a7fcc0c155fb5f082
Md. Sishir Rahman Siam <162951877+shishirRsiam@users.noreply.github.com>
2024-04-05 22:21:49 +0600

    Add files via upload
    ok

---
+++ Introduction to C Programming/Assignment 01/Divisible.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int n; scanf("%d", &n);
+    if(n%3==0) printf("%s", "YES");
+    else printf("%s", "NO");
+    return 0;
+}
---
+++ Introduction to C Programming/Assignment 01/Multiply.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    long long a,b;
+    scanf("%lld %lld", &a, &b);
+    printf("%lld", a*b);
+    return 0;
+}
